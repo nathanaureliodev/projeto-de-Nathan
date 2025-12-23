440ba7a43e407287d11dd9cd7d191912cb99c16a
Nathan <nathanaurelio614@gmail.com>
2025-12-22 21:57:31 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,16 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile-img")
+
+  //substituir a imagem//
+  if (html.classList.contains("light")) {
+    //se estiver light mode,adicionar a imagem light//
+    img.setAttribute("src", "assets/macaco_laranja-circular.png")
+  } else {
+    //se estiver sem o light mode,manter a imagem normal//
+    img.setAttribute("src", "assets/imagem_cortada_circular.png")
+  }
+}
